8f4b2e09223029f520690ad6f953a8a3966ea9f7
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-10-20 12:28:28 -0500

    fixing build issues

--- src/app/hey/page.tsx
+++ src/app/hey/page.tsx
@@ -1,4 +1,4 @@
-export default function () {
+export default function heyPage () {
     return (
         <h1>hey</h1>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return (<h1>Hello you've made it to my portfolio website, thank you for visiting, this is still in progress</h1>)
+  return (
+  <h1>hello
+    </h1>
+  )
 }
