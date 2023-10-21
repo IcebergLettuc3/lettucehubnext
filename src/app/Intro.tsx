748dd976a71f7a155dace3c04a3848f07f8e984c
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-10-21 01:45:25 -0500

    adding intro

---
+++ src/app/Intro.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Intro: React.FC = () => {
+    return (
+        <div className='flex h-screen m-auto flex-col items-center'>
+            <h1 className='text-xl'>
+                Hello my Name is Jonathan Eisenbrandt and this is my website that I've created
+            </h1>
+            <p>
+                contact me: jeisenbrandt1@gmail.com
+            </p>
+        </div>
+    )
+}
+
+export default Intro
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Portfolio from './Portfolio'
+import Intro from './Intro'
 
 export default function Home() {
   return (
   <div>
-    <Portfolio></Portfolio>
+    <Intro/>
+    <Portfolio/>
     </div>
   )
 }
