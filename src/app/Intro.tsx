07ef5b7bb8498573fd6b7035a68f8d9dc493d523
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-10-21 10:36:20 -0500

    fixing appostrophy again "

--- src/app/Intro.tsx
+++ src/app/Intro.tsx
@@ -4,7 +4,7 @@ const Intro: React.FC = () => {
     return (
         <div className='flex m-auto flex-col items-center'>
             <h1 className='text-xl'>
-                Hello my Name is Jonathan Eisenbrandt I'm a software developer with over 1 year of experience and this is my website that I&apos;ve created
+                Hello my Name is Jonathan Eisenbrandt I&apos;m a software developer with over 1 year of experience and this is my website that I&apos;ve created
             </h1>
             <p>
                 contact me: 
