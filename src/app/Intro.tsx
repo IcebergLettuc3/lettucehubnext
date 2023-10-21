c5365b26aa5586f1fc06ee232b1ce76a681da20e
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-10-21 01:46:34 -0500

    removing excess space

--- src/app/Intro.tsx
+++ src/app/Intro.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Intro: React.FC = () => {
     return (
-        <div className='flex h-screen m-auto flex-col items-center'>
+        <div className='flex m-auto flex-col items-center'>
             <h1 className='text-xl'>
                 Hello my Name is Jonathan Eisenbrandt and this is my website that I've created
             </h1>
