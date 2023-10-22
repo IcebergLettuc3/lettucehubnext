896e3bf68b086d4fcf98e76310242d7a83d5e749
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-10-22 10:12:41 -0500

    updating links

--- src/app/Portfolio.tsx
+++ src/app/Portfolio.tsx
@@ -4,19 +4,22 @@ import './Portfolio.css'; // Create this CSS file to style your portfolio
 interface Project {
   title: string;
   description: string;
-  link: string;
+  repoLink: string;
+  deployedLink?: string;
 }
 
 const projects: Project[] = [
   {
     title: "Portfolio Web App",
     description: "A Portfolio Web Application to show off my accomplishments, you should deffinately check it out",
-    link: "https://github.com/IcebergLettuc3/lettucehubnext"
+    repoLink: "https://github.com/IcebergLettuc3/lettucehubnext",
+    deployedLink: "https://lettucehub.com"
+    
   },
   {
     title: "Project Wingman",
     description: "A Project that I'm very excited to be making progress on. This project invols the coordination of a large group of different technologies that involve hardware, firmware and software",
-    link: "https://github.com/IcebergLettuc3/wingman"
+    repoLink: "https://github.com/IcebergLettuc3/wingman"
   },
   // Add more projects as needed
 ];
@@ -31,8 +34,11 @@ const Portfolio: React.FC = () => {
           <li key={index}>
             <h2>{project.title}</h2>
             <p>{project.description}</p>
-            <a href={project.link} target="_blank" rel="noopener noreferrer">
-              View Project
+            <a href={project.repoLink} target="_blank" rel="noopener noreferrer">
+              View Repository
+            </a>
+            <a href={project.deployedLink} target="_blank" rel="noopener noreferrer">
+              View Porject
             </a>
           </li>
         ))}
