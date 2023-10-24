b8d1e2a4310c4025013275109114f62616f0110d
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-10-24 00:28:54 -0500

    text structure change

--- src/app/Intro.tsx
+++ src/app/Intro.tsx
@@ -4,7 +4,11 @@ const Intro: React.FC = () => {
     return (
         <div className='flex m-auto flex-col items-center'>
             <h1 className='text-xl'>
-                Hello my Name is Jonathan Eisenbrandt I&apos;m a software developer with over 1 year of experience and this is my website that I&apos;ve created
+                {/* add conditional int on years of experience */}
+                Hello my Name is Jonathan Eisenbrandt I&apos;m a software developer with over 1 year of experience developing applications for desktop and web.
+            </h1>
+            <h1 className='text-xl'>
+                Welcome to my website.
             </h1>
             <p>
                 contact me: 
