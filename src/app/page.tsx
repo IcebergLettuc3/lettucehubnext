fca2413747008d027c61977928393d72a676336b
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-10-28 12:29:24 -0500

    adding navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Portfolio from './Portfolio'
 import Intro from './Intro'
 import Link from 'next/link'
 import HeroSection from './components/HeroSection'
+import { NavBar } from './components/NavBar'
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4'>
-      <div className='container mx-auto px-12 py-4'>
+      <NavBar/>
+      <div className='container mt-24 mx-auto px-12 py-4'>
         <HeroSection/>
       </div>
     </main>
