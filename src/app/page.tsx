83b9874439148bfc0e37231eae5a6d2e35a34f21
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-10-27 23:30:46 -0500

    overhauling home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,27 @@ import Image from 'next/image'
 import Portfolio from './Portfolio'
 import Intro from './Intro'
 import Link from 'next/link'
+import HeroSection from './components/HeroSection'
 
 export default function Home() {
   return (
-  <div className='flex flex-col items-center justify-center'>
-    <Intro/>
-    <Image
-      className='flex m-auto flex-col items-center'
-      src="/me.jpg"
-      alt="Me"
-      width={500}
-      height={300}
-    />
-    <Link href="/about">About</Link>
-    <Link href="/blog">Blog</Link>
-    <Portfolio/>
-    </div>
+    <main className='flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4'>
+      <div className='container mx-auto px-12 py-4'>
+        <HeroSection/>
+      </div>
+    </main>
+  // <div className='flex flex-col items-center justify-center'>
+  //   <Intro/>
+  //   <Image
+  //     className='flex m-auto flex-col items-center'
+  //     src="/me.jpg"
+  //     alt="Me"
+  //     width={500}
+  //     height={300}
+  //   />
+  //   <Link href="/about">About</Link>
+  //   <Link href="/blog">Blog</Link>
+  //   <Portfolio/>
+  //   </div>
   )
 }
