60fa856a287411b8ce83475f4410c4045e03632c
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-10-27 23:48:35 -0500

    adding goofy littel log statement

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -5,6 +5,9 @@ import {TypeAnimation} from 'react-type-animation'
 
 
 const HeroSection = () => {
+    const handelClick = () => {
+        console.log("button does't work yet")
+    }
     return(
         <section>
             <div className="grid grid-cols-1 sm:grid-cols-12">
@@ -32,11 +35,16 @@ const HeroSection = () => {
                         rip about paragraph from linked in and put it here
                     </p>
                     <div>
-                        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+                        <button
+                            className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white"
+                            onClick={handelClick}>
                             hire me
                         </button>
-                        <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+                        <button
+                            className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 to-pink-500 hover:bg-slate-800 text-white mt-3"
+                            onClick={handelClick}>
                             <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download Resume</span>
+                            
                         </button>
                     </div>
                 </div>
