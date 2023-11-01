c07d0167e89a830d9cc55afb8d3e3ae80881a22e
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-11-01 16:03:02 -0500

    adding title and fixing image reference

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href='/images/icebergLettuce.ico'></link>
+        <title>Lettuce Hub</title>
+        <link rel="icon" href='/images/icebergLettuce.ico'/>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
