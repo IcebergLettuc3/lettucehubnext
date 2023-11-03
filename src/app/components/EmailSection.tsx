62c2d9c09b588f1c27e7457d98fd71e3744d6551
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-11-02 23:10:21 -0500

    resolving error with email send

--- src/app/components/EmailSection.tsx
+++ src/app/components/EmailSection.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image'
 function EmailSection() {
   const [emailSubmitted, setEmailSubmitted] = useState(false)
   const handelSubmit =async (e:any) => {
+    console.log("handelSubmit")
     e.preventDefault();
     const data = {
       email: e.target.email.value,
@@ -29,6 +30,8 @@ function EmailSection() {
     if (response.status === 200) {
       console.log('Message sent.')
       setEmailSubmitted(true)
+    } else {
+      console.log('message not sent')
     }
   }
   return (
