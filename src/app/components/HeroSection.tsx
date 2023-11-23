93b59d4a3cf6b8dbfe8971b6f9f6b9d9538ad548
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-11-23 15:55:23 -0600

    fixing the download button color

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -58,7 +58,7 @@ const HeroSection = () => {
                             Contact Me
                         </button>
                         <button
-                            className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+                            className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
                             onClick={handleClick}>
                             <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download Resume</span>
                             
