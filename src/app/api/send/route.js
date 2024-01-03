81df42ace7790b026cfea7488cf3d14a86b3050d
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2024-01-02 23:39:04 -0600

    i think i fixed the email section

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -13,7 +13,7 @@ export async function POST(req, res) {
   try {
     const data = await resend.emails.send({
       from: fromEmail,
-      to: ['example@gmail.com', email, fromEmail],
+      to: [email],
       subject: subject,
       react: (
         <>
